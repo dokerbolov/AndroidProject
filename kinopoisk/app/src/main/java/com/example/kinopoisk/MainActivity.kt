f32a3776f102cd2750c7d1a3296f6a3cd707afc3
dokerbolov <proface19@gmail.com>
2020-04-11 21:21:24 +0600

    first

--- kinopoisk/app/src/main/java/com/example/kinopoisk/MainActivity.kt
+++ kinopoisk/app/src/main/java/com/example/kinopoisk/MainActivity.kt
@@ -2,11 +2,15 @@ package com.example.kinopoisk
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentTransaction
 import com.google.android.material.bottomnavigation.BottomNavigationItemView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var homeFragment: HomeFragment;
+    lateinit var favoriteFragment: FavoriteFragment;
+    lateinit var userFragment: UserFragment;
 
     lateinit var bottomNavigation: BottomNavigationView
 
@@ -15,7 +19,42 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         bottomNavigation = findViewById(R.id.bottom_navigation)
 
-        bottomNavigation.setOnNavigationItemReselectedListener {  }
+        homeFragment = HomeFragment()
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.frame_layout, homeFragment)
+            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+            .commit()
+
+        bottomNavigation.setOnNavigationItemReselectedListener {
+            item -> when(item.itemId){
+                R.id.home -> {
+                    homeFragment = HomeFragment()
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.frame_layout, homeFragment)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit()
+                }
+                R.id.user -> {
+                    userFragment = UserFragment()
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.frame_layout, userFragment)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit()
+                }
+                R.id.favorite -> {
+                    favoriteFragment = FavoriteFragment()
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.frame_layout, favoriteFragment)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit()
+            }
+        }
+            true
+        }
 
     }
 
