ac8ef6de382dc5d7bf0ea3fccfe5510759c7e489
dokerbolov <proface19@gmail.com>
2020-04-11 15:13:35 +0600

    kinopoisk

---
+++ kinopoisk/app/src/main/java/com/example/kinopoisk/FavoriteActivity.kt
@@ -0,0 +1,12 @@
+package com.example.kinopoisk
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FavoriteActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activiry_favorite)
+    }
+}
---
+++ kinopoisk/app/src/main/java/com/example/kinopoisk/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.kinopoisk
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+
+    lateinit var bottomNavigation: BottomNavigationView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        bottomNavigation = findViewById(R.id.bottom_navigation)
+
+        bottomNavigation.setOnNavigationItemReselectedListener {  }
+
+    }
+
+}
