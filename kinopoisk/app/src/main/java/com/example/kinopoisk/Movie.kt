e5b8c48202a570dbbacdd2151a2ad4aa8c2536d2
dokerbolov <proface19@gmail.com>
2020-04-18 16:25:22 +0600

    db

---
+++ kinopoisk/app/src/main/java/com/example/kinopoisk/Movie.kt
@@ -0,0 +1,4 @@
+package com.example.kinopoisk
+
+class Movie {
+}
--- kinopoisk/app/src/main/java/com/example/kinopoisk/User.kt
+++
@@ -1,7 +0,0 @@
-package com.example.kinopoisk
-
-data class User(
-    val name: String,
-    val lastName: String,
-    val email: String
-)
